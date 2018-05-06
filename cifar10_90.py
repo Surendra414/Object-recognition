7d24099408ee3618302120e8a21565e938ceaca4
Surendra Shringi <sshringi003@gmail.com>
2018-05-06 14:33:34 +0530

    Update cifar10_90.py

--- cifar10_90.py
+++ cifar10_90.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 17 12:01:13 2017
+Created on SUn May 08 14:33:13 2018
 
-@author: abhijeet
+@author: Surendra Shringi
 """
 
 import keras
